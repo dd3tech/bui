43d35c8e656ee0b92dadeff369a3796d77190a24
tbor00 <timoteo.borgobello@dd3tech.mx>
2021-11-11 16:29:50 -0300

    Delete node_modules

--- node_modules/@storybook/addon-controls/preset.js
+++
@@ -1,8 +0,0 @@
-const { ensureDocsBeforeControls } = require('./dist/cjs/preset/ensureDocsBeforeControls');
-
-function managerEntries(entry = [], options) {
-  ensureDocsBeforeControls(options.configDir);
-  return [...entry, require.resolve('./dist/esm/register')];
-}
-
-module.exports = { managerEntries };
