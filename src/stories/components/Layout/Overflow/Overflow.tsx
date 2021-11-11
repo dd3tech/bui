eabba014168f751f9647ef2482f6099e4eb2810f
tbor00 <timoteo.borgobello@dd3tech.mx>
2021-11-11 16:27:20 -0300

    add overflow

---
+++ src/stories/components/Layout/Overflow/Overflow.tsx
@@ -0,0 +1,13 @@
+interface OverFlowProps {
+    children?: React.ReactNode
+    className?: string
+    overflow?: 'auto' | 'hidden' | 'visible' | 'x-auto' | 'x-hidden' | 'x-visible' | 'scroll' | 'y-auto' | 'y-hidden' | 'y-visible' | 'x-scroll' | 'y-scroll'
+}
+
+export const OverFlow = ({ children, className, overflow, ...props }: OverFlowProps) => {
+    return (
+        <div className={`overflow-${overflow ?? 'auto'} ${className}`} {...props}>
+            {children}
+        </div>
+    )
+}
