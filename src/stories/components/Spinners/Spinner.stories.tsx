9aeb2d91930d7f904f122c7a828eff042c237ec9
tbor00 <timoteo.borgobello@dd3tech.mx>
2021-11-11 15:26:54 -0300

    Add spinner component

---
+++ src/stories/components/Spinners/Spinner.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Spinner as SpinnerComponent } from './Spinner'
+
+export default {
+    title: 'Design System/Spinner'
+} as ComponentMeta<typeof SpinnerComponent>
+
+const Template: ComponentStory<typeof SpinnerComponent> = (args) => <SpinnerComponent {...args} />
+
+export const Grow = Template.bind({})
+
+Grow.args = {
+    type: 'grow',
+    color: '#f51'
+}
+
+export const Circle = Template.bind({})
+
+Circle.args = {
+    type: 'circle',
+    color: '#f51'
+}
+
+export const Red = Template.bind({})
+
+Red.args = {
+    variant: 'red'
+}
+
+export const Blue = Template.bind({})
+
+Blue.args = {
+    variant: 'blue'
+}
+export const Yellow = Template.bind({})
+
+Yellow.args = {
+    variant: 'yellow'
+}
+export const Green = Template.bind({})
+
+Green.args = {
+    variant: 'green'
+}
+
+export const Custom = Template.bind({})
+
+Custom.args = {
+    color: '#f51'
+}
