b92820c7f17a25c028f0091b51f4e26ecc43c284
tbor00 <timoteo.borgobello@dd3tech.mx>
2021-11-11 20:15:37 -0300

    Add example Button icon

--- src/stories/components/ImageIcon/ImageIcon.stories.tsx
+++ src/stories/components/ImageIcon/ImageIcon.stories.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { ImageIcon as ImageIconComponent } from './ImageIcon'
+import { h2 } from '../Typography/Text/Text.stories'
 
 export default {
     title: 'Design System/imageIcon'
 } as ComponentMeta<typeof ImageIconComponent>
 
 const Template: ComponentStory<typeof ImageIconComponent> = (args) => <ImageIconComponent {...args} />
+
+export const Example = () => {
+    return (
+        <>
+            <div className="flex">
+                <div className="mr-5">
+                    <h2>Image Icon</h2>
+                    <ImageIconComponent src="https://picsum.photos/200/300?grayscale" />
+                </div>
+                <div className="mr-5">
+                    <h2>Button Image Icon</h2>
+                    <ImageIconComponent src="https://picsum.photos/200/300?grayscale" button buttonOnClick={() => alert('Button')} />
+                </div>
+            </div>
+        </>
+    )
+}
 
 export const ImageIcon = Template.bind({})
 ImageIcon.args = {
