fc37b810817070feea2ac732b98d67677d5ead10
tbor00 <timoteo.borgobello@dd3tech.mx>
2021-11-12 13:36:00 -0300

    Added placeholder

---
+++ src/stories/components/Placeholder/PlaceHolder.tsx
@@ -0,0 +1,12 @@
+import './placeholder.css'
+
+interface PlaceHolderProps {
+    size?: 'small' | 'medium' | 'large'
+    backgroundColor?: string
+    className?: string
+}
+
+export const PlaceHolder = ({ size = 'large', backgroundColor, className, ...props }: PlaceHolderProps) => {
+    const sizeClass = size ? `storybook-placeholder-${size}` : ''
+    return <span className={`${className ?? ''} ${sizeClass} storybook-placeholder`} style={{ backgroundColor }} {...props}></span>
+}
