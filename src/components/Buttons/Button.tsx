54567c0a4236a17f79c543688b1aa818dfd92865
Jasan Hernandez <jasan.hernandez@dd3tech.mx>
2022-06-20 22:08:38 -0500

    [update] Button and Badge

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,19 +1,81 @@
-import React from 'react'
-import './button.css'
+import { ReactNode, MouseEventHandler } from 'react'
+import { Spinner } from '../Spinners'
 
-interface ButtonProps {
-    primary?: boolean
-    backgroundColor?: string
+interface IButtonProps {
+    variant?: 'primary' | 'secondary' | 'tertiary' | 'outline' | 'link' | 'ghost' | 'cancel' | 'success' | 'outlineWhite' | 'outlineBlue'
     size?: 'small' | 'medium' | 'large'
-    onClick?: () => void
-    children?: React.ReactNode
+    padding?: number
+    disabled?: boolean
+    onClick?: MouseEventHandler<HTMLButtonElement>
+    children?: ReactNode
+    isLoading?: boolean
+    className?: string
+    paddingX?: number
+    paddingY?: number
+    textLoading?: string
 }
 
-export const Button = ({ primary = false, size = 'medium', backgroundColor, children, ...props }: ButtonProps) => {
-    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
+export const Button = ({
+    variant = 'primary',
+    size = 'medium',
+    onClick,
+    children,
+    isLoading,
+    className = '',
+    padding,
+    paddingX,
+    paddingY,
+    textLoading,
+    ...props
+}: IButtonProps) => {
+    const buttonsVariants: { [key: string]: string } = {
+        primary: 'bg-blue-700 hover:bg-blue-800 text-white disabled:bg-gray-300',
+        secondary: 'bg-transparent border border-black hover:bg-white disabled:opacity-20',
+        cancel: 'bg-white text-black hover:text-white hover:bg-red-500 disabled:opacity-75',
+        outlineBlue: 'bg-transparent border border-blue-700 text-blue-700',
+        success: 'bg-green-500 hover:bg-green-600 text-white',
+        outlineWhite: 'bg-transparent border border-white  text-white  hover:bg-gray-50 hover:text-black'
+    }
+
+    const sizeVariants: { [key: string]: string } = {
+        large: 'rounded-lg w-auto'
+    }
+
+    const buttonPading = () => {
+        if (paddingX && paddingY) {
+            return `px-${paddingX} py-${paddingY}`
+        }
+
+        if (paddingX) {
+            return `px-${paddingX}`
+        }
+
+        if (paddingY) {
+            return `py-${paddingY}`
+        }
+
+        if (padding) {
+            return `p-${padding}`
+        }
+
+        return 'p-2'
+    }
+
     return (
-        <button type="button" className={['storybook-button', `storybook-button--${size}`, mode].join(' ')} style={{ backgroundColor }} {...props}>
-            {children}
+        <button
+            className={`rounded-md ${buttonPading()} font-bold transition duration-500 ease-out hover:ease-in ${buttonsVariants[variant]} ${
+                sizeVariants[size]
+            } ${className}`}
+            onClick={onClick}
+            {...props}
+        >
+            {isLoading ? (
+                <span className="flex items-center gap-2 justify-center">
+                    <Spinner /> {textLoading}
+                </span>
+            ) : (
+                children
+            )}
         </button>
     )
 }
