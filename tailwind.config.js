26f023f21b014cbc8292b4d050a6355cdf93dbbb
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-07-26 20:11:58 -0300

    add content on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
 }
