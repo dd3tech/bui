1836f3b7b60dd7f1c990bf4b84ebabbdf0badd94
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-07-29 21:36:15 -0300

    Feat: component stepper

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,15 +1,37 @@
-export const Stepper = ({ phase, totalPhases }: { phase: number; totalPhases: number }) => {
-    const style = phase !== totalPhases ? { borderRightColor: '#1d4ed8', borderBottomColor: '#1d4ed8' } : { borderColor: '#1d4ed8' }
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+
+interface StepperProps {
+    phase: number
+    totalPhases: number
+    width?: string
+    height?: string
+    fontSize?: string
+    textColor?: string
+    strokeWidth?: number
+    strokeColor?: string
+    text?: string
+}
+
+export const Stepper = ({ phase, totalPhases, width, height, ...props }: StepperProps) => {
+    const valuePercentage = Math.round((100 / totalPhases) * phase)
 
     return (
-        <div className="container mx-auto">
-            <div className="flex justify-center items-center">
-                <div style={{ ...style, borderWidth: '5px' }} className="rounded-full h-16 w-16 border-gray-300 flex items-center justify-center">
-                    <div className="text-center left-5 text-blue-700 bold text-xs">
-                        {phase} / {totalPhases}
-                    </div>
-                </div>
-            </div>
+        <div style={{ width, height }}>
+            <CircularProgressbar
+                styles={{
+                    text: {
+                        fill: props.textColor ?? '#1d4ed8',
+                        fontSize: props.fontSize ?? ''
+                    },
+                    path: {
+                        stroke: props.strokeColor ?? '#1d4ed8'
+                    }
+                }}
+                strokeWidth={props.strokeWidth}
+                value={valuePercentage}
+                text={props.text ?? `${phase}/${totalPhases}`}
+            />
         </div>
     )
 }
