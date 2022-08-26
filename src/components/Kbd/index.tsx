2b0a1cdf8896fa75bd7c4fa32923899cb103faf8
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-08-26 18:24:08 -0300

    test: 💍 Kbd component was tested
    the correct operation of the Kbd component was verified

--- src/components/Kbd/index.tsx
+++ src/components/Kbd/index.tsx
@@ -20,7 +20,7 @@ export function Kbd({ kbds, separator }: KBDProps) {
                     <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
                         {text}
                     </kbd>{' '}
-                    {!isLastKbd(index) && <>{separator ?? '+'}</>}{' '}
+                    <span> {!isLastKbd(index) && <>{separator ?? '+'}</>} </span>
                 </React.Fragment>
             ))}
         </p>
