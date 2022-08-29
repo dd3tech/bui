6a5d2ce51d7dc330efbbd6aa13d2450fa6c212b1
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-08-29 19:49:06 -0300

    test: 💍 ModalCustom test was refactorized
    Change the structure of test

--- tests/ModalCustom.spec.tsx
+++ tests/ModalCustom.spec.tsx
@@ -3,50 +3,48 @@ import { it, describe, expect, vi } from 'vitest'
 import { render, fireEvent, RenderResult } from '@testing-library/react'
 import { ModalCustom } from '../src/components'
 
-describe('Component UI: ModalCustom', () => {
+describe('ModalCustom component', () => {
     const setCloseModal = vi.fn()
     let renderResult: RenderResult
 
     beforeEach(() => {
-        const result = render(<ModalCustom active={false} setCloseModal={setCloseModal} />)
-        renderResult = result
+        renderResult = render(<ModalCustom active={false} setCloseModal={setCloseModal} />)
     })
 
-    it('ModalCustom is closed', () => {
+    it('the modal is closed', () => {
         expect(renderResult.container.firstChild).toBeNull()
     })
 
-    it('ModalCustom, pass the children is working', () => {
+    it('modal, pass the children is working', () => {
         renderResult.rerender(
             <ModalCustom active={true} setCloseModal={setCloseModal}>
                 <h1>Children</h1>
             </ModalCustom>
         )
-
-        expect(renderResult.getByText('Children')).toBeDefined()
+        expect(renderResult.container.firstChild).toBeDefined()
     })
 
-    it('ModalCustom active is working', () => {
+    it('the modal is open', () => {
         renderResult.rerender(<ModalCustom active={true} setCloseModal={setCloseModal} />)
         const modal = renderResult.getByRole('modal-custom')
         expect(modal.className).toContain('fixed')
     })
 
-    it('ModalCustom, widht and height props is working', () => {
+    it('modal widht and height props is working', () => {
         renderResult.rerender(<ModalCustom active={true} setCloseModal={setCloseModal} width="50%" height="30%" />)
         const modal = renderResult.getByTestId('modal-contain')
         expect(modal.style.width).toBe('50%')
         expect(modal.style.height).toBe('30%')
     })
 
-    it('ModalCustom, if keyUp is escape, close modal', () => {
+    it('close modal if press esc key', () => {
         renderResult.rerender(<ModalCustom active={true} setCloseModal={setCloseModal} />)
         const modal = renderResult.getByRole('modal-custom')
         fireEvent.keyUp(modal, { key: 'Escape' })
         expect(renderResult.container.firstChild).toBeNull()
     })
 
-    it('ModalCustom, close this when click the button (X)', () => {
+    it('close modal when click the button(x)', () => {
         renderResult.rerender(<ModalCustom active={true} setCloseModal={setCloseModal} />)
         const btnCancel = renderResult.getByRole('btn-close')
         fireEvent.click(btnCancel)
