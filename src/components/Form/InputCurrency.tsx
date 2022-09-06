be1994364d34954ebc4a4d30800b751bb92c7b33
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-09-06 01:57:22 -0300

    feat: 🎸 InputCurrency component props are changed

--- src/components/Form/InputCurrency.tsx
+++ src/components/Form/InputCurrency.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, useEffect, FC } from 'react'
-import { getValueWithDecimalFormat, inputCurrency } from 'dd360-utils'
+import { getValueWithDecimalFormat, format } from 'dd360-utils'
 import { getInputClassName, variantIs } from './shared'
 
 export interface InputCurrencyProps extends React.HTMLProps<HTMLInputElement> {
@@ -14,54 +14,52 @@ export interface InputCurrencyProps extends React.HTMLProps<HTMLInputElement> {
     rounded?: 'sm' | 'md' | 'full'
 }
 
-const InputCurrency: FC<InputCurrencyProps> = forwardRef<HTMLInputElement, InputCurrencyProps>(
-    ({ success, error, onChangeCurrency, value, useSymbol, rounded, variant = 'active', helperText, className, ...otherProps }: InputCurrencyProps) => {
-        const { format } = inputCurrency
-        const formatProps = { removeCents: false, removeCommas: false, removeSymbol: !useSymbol }
-        const { input: inputClassName, message: messageClassName } = getInputClassName({
-            variant: variantIs({ error, success, variant }),
-            rounded: rounded ?? 'md',
-            border: true
-        })
+const InputCurrency: FC<InputCurrencyProps> = forwardRef<HTMLInputElement, InputCurrencyProps>((iCurrencyProps: InputCurrencyProps) => {
+    const { success, error, onChangeCurrency, value, useSymbol, rounded, variant = 'active', helperText, className, ...otherProps } = iCurrencyProps
+    const formatProps = { removeCents: false, removeCommas: false, removeSymbol: !useSymbol }
+    const { input: inputClassName, message: messageClassName } = getInputClassName({
+        variant: variantIs({ error, success, variant }),
+        rounded: rounded ?? 'md',
+        border: true
+    })
 
-        function inputOnChange({ target }: { target: HTMLInputElement }) {
-            if (onChangeCurrency) {
-                let formattedValue = useSymbol ? `$${getValueWithDecimalFormat(target.value)}` : getValueWithDecimalFormat(target.value)
-                onChangeCurrency(formattedValue)
-            }
+    function inputOnChange({ target }: { target: HTMLInputElement }) {
+        if (onChangeCurrency) {
+            let formattedValue = useSymbol ? `$${getValueWithDecimalFormat(target.value)}` : getValueWithDecimalFormat(target.value)
+            onChangeCurrency(formattedValue)
         }
-
-        function onBlurCurrency(e: React.FocusEvent<HTMLInputElement>) {
-            if (onChangeCurrency) {
-                const formattedValue = useSymbol ? format(e.target.value, formatProps) : format(e.target.value).replace(/[$]/g, '')
-                onChangeCurrency(formattedValue)
-            }
-            if (otherProps.onBlur) {
-                otherProps.onBlur(e)
-            }
-        }
-
-        useEffect(() => {
-            if (onChangeCurrency) {
-                const formattedValue = useSymbol ? format(value, formatProps) : format(value).replace(/[$]/g, '')
-                onChangeCurrency(formattedValue)
-            }
-        }, [useSymbol])
-
-        return (
-            <>
-                <input
-                    style={{ outline: 'none' }}
-                    className={`${className ?? ''} ${inputClassName}`}
-                    onChange={inputOnChange}
-                    onBlur={onBlurCurrency}
-                    value={value}
-                    {...otherProps}
-                />
-                {helperText && <small className={`${messageClassName} ${otherProps.classNameHelperText ?? ''}`}>{helperText}</small>}
-            </>
-        )
     }
-)
+
+    function onBlurCurrency(e: React.FocusEvent<HTMLInputElement>) {
+        if (onChangeCurrency) {
+            const formattedValue = useSymbol ? format(e.target.value, formatProps) : format(e.target.value).replace(/[$]/g, '')
+            onChangeCurrency(formattedValue)
+        }
+        if (otherProps.onBlur) {
+            otherProps.onBlur(e)
+        }
+    }
+
+    useEffect(() => {
+        if (onChangeCurrency) {
+            const formattedValue = useSymbol ? format(value, formatProps) : format(value).replace(/[$]/g, '')
+            onChangeCurrency(formattedValue)
+        }
+    }, [useSymbol, onChangeCurrency])
+
+    return (
+        <>
+            <input
+                style={{ outline: 'none' }}
+                className={`${className ?? ''} ${inputClassName}`}
+                onChange={inputOnChange}
+                onBlur={onBlurCurrency}
+                value={value}
+                {...otherProps}
+            />
+            {helperText && <small className={`${messageClassName} ${otherProps.classNameHelperText ?? ''}`}>{helperText}</small>}
+        </>
+    )
+})
 
 export default InputCurrency
--- src/stories/InputCurrency.stories.tsx
+++ src/stories/InputCurrency.stories.tsx
@@ -1,79 +1,21 @@
 import React from 'react'
-import { ComponentMeta } from '@storybook/react'
-import InputCurrency from '../components/Form/InputCurrency'
+import { ComponentMeta, Story } from '@storybook/react'
+import InputCurrency, { InputCurrencyProps } from '../components/Form/InputCurrency'
 import { format } from 'dd360-utils'
 
 export default {
     title: 'Form/InputCurrency'
 } as ComponentMeta<typeof InputCurrency>
 
-export const Default = () => {
-    const [values, setValues] = React.useState({
-        currency: format('32500'),
-        withoutSymbol: format('4200000.32').replace(/[$]/g, ''),
-        error: format('752500'),
-        succes: format('39800')
-    })
+const Template: Story<InputCurrencyProps> = (args: InputCurrencyProps) => {
+    const [value, setValue] = React.useState(format('32500'))
 
-    function changeValue(name: string, value: string) {
-        setValues({ ...values, [name]: value })
-    }
+    return <InputCurrency value={value} onChangeCurrency={(n) => setValue(n)} {...args} />
+}
 
-    const onChangeValue = {
-        changeCurrency: (n: string) => changeValue('currency', n),
-        changeSymbol: (n: string) => changeValue('withoutSymbol', n),
-        changeError: (n: string) => changeValue('error', n),
-        changeSuccess: (n: string) => changeValue('succes', n)
-    }
-
-    return (
-        <>
-            <div className="my-20 px-8 grid grid-cols-3 gap-20 justify-20 container mx-auto">
-                <div className="w-72">
-                    <h2 className="text-xl font-bold">Input Currency</h2>
-                    <InputCurrency
-                        className="mt-2"
-                        value={values.currency}
-                        onChangeCurrency={onChangeValue.changeCurrency}
-                        useSymbol
-                        helperText="Este es el helperText"
-                    />
-                </div>
-
-                <div className="w-72">
-                    <h2 className="text-xl font-bold">Without helperText</h2>
-                    <InputCurrency value={values.currency} />
-                </div>
-
-                <div className="w-72">
-                    <h2 className="text-xl font-bold">Without Symbol</h2>
-                    <InputCurrency className="mt-2" value={values.withoutSymbol} onChangeCurrency={onChangeValue.changeSymbol} helperText="Sin symbol" />
-                </div>
-
-                <div className="w-72">
-                    <h2 className="text-xl font-bold">Error</h2>
-                    <InputCurrency
-                        className="mt-2"
-                        error
-                        onChangeCurrency={onChangeValue.changeError}
-                        value={values.error}
-                        useSymbol
-                        helperText="Oops! Hubo un error"
-                    />
-                </div>
-
-                <div className="w-72">
-                    <h2 className="text-xl font-bold">Success</h2>
-                    <InputCurrency
-                        className="mt-2"
-                        success
-                        value={values.succes}
-                        onChangeCurrency={onChangeValue.changeSuccess}
-                        useSymbol
-                        helperText="Su saldo se guardó exitosamente!"
-                    />
-                </div>
-            </div>
-        </>
-    )
+export const Default = Template.bind({})
+Default.args = {
+    className: 'mt-2',
+    useSymbol: true,
+    helperText: 'Su saldo se guardó exitosamente!'
 }
