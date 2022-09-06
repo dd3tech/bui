76a82eb012c9f4f99a7ba213aac9ff6783b9b7b8
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-09-05 21:12:15 -0300

    fix: 🐛 import of the Card component was fixed

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { Card as CardComponent } from '../components/Card'
+import CardComponent from '../components/Card'
 import { Button } from '../components'
 import Text from '../components/Typography'
 
