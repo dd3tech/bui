800526af4f08fd6cd6433d658fdadf87e66f1410
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-12-07 16:32:27 -0300

    test(components): 💍 change structure of test and add more use cases

--- tests/components/Badge.test.tsx
+++ tests/components/Badge.test.tsx
@@ -1,54 +1,74 @@
 import React from 'react'
 import { it, describe } from 'vitest'
-import { render, waitFor, cleanup } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
+
 import { Badge } from '../../src/components'
 
-const badgeContainer = (getByRole) => {
-    return getByRole('container-badge')
-}
+const badgeContainer = 'container-badge'
 
-describe('Component UI: Badge', () => {
-    afterEach(() => {
-        cleanup()
+describe('<Badge/>', () => {
+    it('should be render', () => {
+        const { container } = render(<Badge />)
+        expect(container.firstChild).toBeDefined()
     })
 
+    it('the prop className works correctly ', () => {
+        const { getByRole } = render(<Badge variant="primary" icon="tag" className="text-red-600" />)
+        expect(getByRole(badgeContainer).className).toContain('text-red-600')
+    })
+
+    it('the prop classNameIcon works correctly ', () => {
+        const { getByRole } = render(<Badge variant="primary" icon="tag" classNameIcon="text-red-600" />)
+        const classNameIconElement = getByRole(badgeContainer).firstChild as HTMLDivElement
+        expect(classNameIconElement.getAttribute('class')).toBe('text-red-600')
+    })
+})
+
+describe('render with different variants', () => {
     it('Badge contains the className corresponding to the warning variant', () => {
         const { getByRole } = render(<Badge variant="warning" icon="success" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-yellow-100 border border-yellow-500 text-gray-500')
+        expect(getByRole(badgeContainer).className).toContain('bg-yellow-100 border border-yellow-500 text-gray-500')
     })
 
     it('Badge contains the className corresponding to the infoPrimary variant ', () => {
         const { getByRole } = render(<Badge variant="infoPrimary" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-blue-50 border border-blue-300 text-gray-500')
+        expect(getByRole(badgeContainer).className).toContain('bg-blue-50 border border-blue-300 text-gray-500')
     })
 
     it('Badge contains the className corresponding to the infoSecondary variant ', () => {
         const { getByRole } = render(<Badge variant="infoSecondary" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-transparent border border-blue-300 text-gray-500')
+        expect(getByRole(badgeContainer).className).toContain('bg-transparent border border-blue-300 text-gray-500')
     })
 
     it('Badge contains the className corresponding to the success variant ', () => {
         const { getByRole } = render(<Badge variant="success" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-green-50 border border-green-300 text-gray-500 font-medium')
+        expect(getByRole(badgeContainer).className).toContain('bg-green-50 border border-green-300 text-gray-500 font-medium')
     })
 
     it('Badge contains the className corresponding to the primary variant ', () => {
         const { getByRole } = render(<Badge variant="primary" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-transparent border border-blue-700 text-blue-700')
+        expect(getByRole(badgeContainer).className).toContain('bg-transparent border border-blue-700 text-blue-700')
     })
 
     it('Badge contains the className corresponding to the secondary variant ', () => {
         const { getByRole } = render(<Badge variant="secondary" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-transparent border border-white text-white hover:bg-gray-50 hover:text-black')
+        expect(getByRole(badgeContainer).className).toContain('bg-transparent border border-white text-white hover:bg-gray-50 hover:text-black')
     })
 
     it('Badge contains the className corresponding to the error variant and the props text works correctly  ', () => {
         const { getByRole } = render(<Badge variant="error" text="Testing" />)
-        expect(badgeContainer(getByRole).className).toContain('bg-red-50 border border-red-300 text-gray-500 font-medium')
-        expect(badgeContainer(getByRole).textContent).contain('Testing')
+        expect(getByRole(badgeContainer).className).toContain('bg-red-50 border border-red-300 text-gray-500 font-medium')
+        expect(getByRole(badgeContainer).textContent).contain('Testing')
+    })
+})
+
+describe('prop: icon', () => {
+    it('if the prop icon not exist, should be rendered with justify-center', () => {
+        const { getByRole } = render(<Badge icon="none" />)
+        expect(getByRole(badgeContainer).className).toContain('justify-center')
     })
 
-    it('Badge, the props icon works correctly ', async () => {
+    it('the props icon works correctly ', async () => {
         const { container, rerender } = render(<Badge variant="success" />)
         expect(container.querySelector(`[id=success]`)).toBeDefined()
         rerender(<Badge variant="primary" icon="tag" />)
@@ -58,13 +78,53 @@ describe('Component UI: Badge', () => {
         expect(container.querySelector(`[id=tag]`)).toBeDefined()
     })
 
-    it('Badge, the props className works correctly ', () => {
-        const { getByRole } = render(<Badge variant="primary" icon="tag" className="text-red-600" />)
-        expect(badgeContainer(getByRole).className).toContain('text-red-600')
+    it('if an icon is not provided, one should be assigned by default', () => {
+        const { getByRole } = render(<Badge variant="warning" />)
+        expect(getByRole(badgeContainer).id).toBe('HomeIcon')
     })
 
-    it('Badge, the props classNameIcon works correctly ', () => {
-        const { getByRole } = render(<Badge variant="primary" icon="tag" classNameIcon="text-red-600" />)
-        expect(badgeContainer(getByRole).firstChild.getAttribute('class')).toBe('text-red-600')
+    it('icon tag should be render', () => {
+        const { getByRole } = render(<Badge icon="tag" />)
+        expect(getByRole(badgeContainer).id).toBe('tag')
+    })
+
+    it('icon clock should be render', () => {
+        const { getByRole } = render(<Badge icon="clock" />)
+        expect(getByRole(badgeContainer).id).toBe('clock')
+    })
+
+    it('icon warning should be render', () => {
+        const { getByRole } = render(<Badge icon="warning" />)
+        expect(getByRole(badgeContainer).id).toBe('warning')
+    })
+
+    it('icon check should be render', () => {
+        const { getByRole } = render(<Badge icon="check" />)
+        expect(getByRole(badgeContainer).id).toBe('check')
+    })
+
+    it('success icon should be render', () => {
+        const { getByRole } = render(<Badge icon="success" />)
+        expect(getByRole(badgeContainer).id).toBe('success')
+    })
+
+    it('exclamation icon works correctly', () => {
+        const { getByRole } = render(<Badge icon="exclamation" />)
+        expect(getByRole(badgeContainer).id).toBe('exclamation')
+    })
+
+    it('icon clipboard-copy should be render', () => {
+        const { getByRole } = render(<Badge icon="clipboard-copy" />)
+        expect(getByRole(badgeContainer).id).toBe('clipboard-copy')
+    })
+
+    it('home icon should be render', () => {
+        const { getByRole } = render(<Badge icon="HomeIcon" />)
+        expect(getByRole(badgeContainer).id).toBe('HomeIcon')
+    })
+
+    it('none icon should be render without icon', () => {
+        const { getByRole } = render(<Badge icon="none" />)
+        expect(getByRole(badgeContainer).id).toBe('none')
     })
 })
