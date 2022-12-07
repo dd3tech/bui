8857dc92d8e803d80f177d2b73b008776f8711c7
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-12-06 22:52:00 -0300

    fix(components): 🐛 add default separator and remove unnecesary fragment

--- src/components/Kbd/Kbd.tsx
+++ src/components/Kbd/Kbd.tsx
@@ -22,7 +22,7 @@ const Kbd = forwardRef<HTMLElement, KBDProps>((props: KBDProps, ref) => {
                     <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
                         {text}
                     </kbd>{' '}
-                    <span> {!isLastKbd(index) && <>{separator ?? '+'}</>} </span>
+                    <span>{!isLastKbd(index) && separator} </span>
                 </React.Fragment>
             ))}
         </p>
