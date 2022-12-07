2a2cd36a6330c316df9661e34aa8c7b7eed69aa7
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-12-06 22:52:45 -0300

    test(components): 💍 refactor test with differents describes

--- tests/components/Kbd.test.tsx
+++ tests/components/Kbd.test.tsx
@@ -1,45 +1,68 @@
 import React from 'react'
 import { it, describe } from 'vitest'
 import { cleanup, render, RenderResult } from '@testing-library/react'
-import { Kbd } from '../../src/components'
+
+import Kbd from '../../src/components/Kbd'
 
 const kbds = ['Ctrl', 'Shift', 'R']
 
-describe('Kbd component', () => {
-    let renderResult: RenderResult
+describe('<Kbd/>: smooth', () => {
+    it('should be render', () => {
+        const { container } = render(<Kbd kbds={kbds} separator="+" />)
+        expect(container).toBeDefined()
+    })
+})
+
+describe('prop: separator', () => {
+    it('the separator renders and works correctly if we change it', () => {
+        const kbd = render(<Kbd kbds={kbds} separator="&" />)
+        expect(kbd.getAllByText('&')).toHaveLength(2)
+
+        kbd.rerender(<Kbd kbds={kbds} separator="-" />)
+        expect(kbd.getAllByText('-')).toHaveLength(2)
+    })
+
+    it('if no separator is passed, it should have one by default', () => {
+        const kbd = render(<Kbd kbds={['1st', '2nd']} />)
+        expect(kbd.getByText('+')).toBeDefined()
+    })
+
+    it('if the kbd is the last one, the separator should not be show', () => {
+        const kbd = render(<Kbd kbds={kbds} separator=">" />)
+        kbds.forEach((k, i) => {
+            const element = kbd.getByText(k)
+            if (kbds.length - 1 === i) {
+                expect(element.nextSibling?.textContent).not.toContain('>')
+                return
+            }
+            expect(element.nextElementSibling?.textContent).toContain('>')
+        })
+    })
+})
+
+describe('prop: kbds', () => {
+    let kbdRenderResult: RenderResult
 
     beforeEach(() => {
-        renderResult = render(<Kbd kbds={kbds} separator="+" />)
+        kbdRenderResult = render(<Kbd kbds={kbds} separator="+" />)
     })
 
     afterEach(() => {
         cleanup()
     })
 
-    it('can be render', () => {
-        expect(renderResult.container).toBeDefined()
-    })
-
-    it('all kbds are correct', () => {
+    it('the kbds should be rendered', () => {
         kbds.forEach((kbdName) => {
-            expect(renderResult.getByText(kbdName)).toBeDefined()
+            expect(kbdRenderResult.getByText(kbdName)).toBeDefined()
         })
     })
 
-    it('the separator renders and works correctly if we change it', () => {
-        expect(renderResult.getAllByText('+')).toHaveLength(2)
-        renderResult.rerender(<Kbd kbds={kbds} separator="-" />)
-
-        expect(renderResult.getAllByText('-')).toHaveLength(2)
-    })
-
-    it('kbd have the HTML kbd tag', () => {
+    it('all kbds should be wrapped by an HTML element', () => {
         const kbdItems: Array<string | unknown> = []
         kbds.find((kdName) => {
-            expect(renderResult.getByText(kdName).nodeName).toBe('KBD')
-            kbdItems.push(renderResult.getByText(kdName).nodeName)
+            expect(kbdRenderResult.getByText(kdName).nodeName).toBe('KBD')
+            kbdItems.push(kbdRenderResult.getByText(kdName).nodeName)
         })
-
         expect(kbdItems).toHaveLength(3)
     })
 })
