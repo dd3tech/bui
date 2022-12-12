afa2f6c99dadb65ff5a585a3506fdcaeb4d2d544
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-12-12 12:16:14 -0300

    refactor(components): 💡 change conditional render and order rounded prop

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-    rounded?: 'sm' | 'lg' | 'md'
+    rounded?: 'sm' | 'md' | 'lg'
     circle?: boolean
     width?: number
     height?: number
@@ -13,12 +13,18 @@ const Image = ({ src, alt, className, rounded, circle, width, height, ...props }
             src={src}
             style={{ height: `${height}px`, width: `${width}px` }}
             alt={alt ?? src}
-            className={`${rounded ? `rounded-${rounded}` : ''} ${className ?? ''} ${circle && 'rounded-50'}`}
+            className={`rounded-${rounded} ${className ?? ''} ${circle ? 'rounded-50' : ''}`}
             {...props}
         />
     )
 }
 
 Image.displayName = 'Image'
+Image.defaultProps = {
+    rounded: 'lg',
+    circle: false,
+    width: 200,
+    height: 200
+}
 
 export default Image
