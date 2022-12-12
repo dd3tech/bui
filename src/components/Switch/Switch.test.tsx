44dc140b9fe9b203c326e485e690c8084b26275f
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-12-12 17:37:26 -0300

    test(components): 💍 add missing case uses of Switch test

--- src/components/Switch/Switch.test.tsx
+++ src/components/Switch/Switch.test.tsx
@@ -1,18 +1,64 @@
 import { it, describe, expect, vi } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { cleanup, fireEvent, render } from '@testing-library/react'
 
 import Switch from './Switch'
 
-describe('<Switch/>', () => {
-    const setToggle = vi.fn()
+const defaultProps = {
+    text: 'Example',
+    toggle: false,
+    setToggle: vi.fn()
+}
 
-    it('Switch is working', () => {
-        render(<Switch toggle={false} setToggle={setToggle} text="Example" />)
-        expect(screen.getByTestId('switch-toggle')).toBeDefined()
+describe('<Switch/>', () => {
+    beforeEach(() => {
+        cleanup()
+        vi.resetAllMocks()
     })
 
-    it('Switch render text', () => {
-        render(<Switch toggle={false} setToggle={setToggle} text="Example" />)
-        expect(screen.getByText('Example')).toBeDefined()
+    it('should be render', () => {
+        const { container } = render(<Switch {...defaultProps} />)
+        expect(container.firstChild).toBeDefined()
+    })
+
+    it('should render with a switch toggle', () => {
+        const { getByTestId } = render(<Switch {...defaultProps} />)
+        expect(getByTestId('switch-toggle')).toBeDefined()
+    })
+
+    it('should render with a prop text', () => {
+        const { getByText } = render(<Switch {...defaultProps} />)
+        expect(getByText('Example')).toBeDefined()
+    })
+
+    it('when we click on the switch should change state', () => {
+        const { getByTestId } = render(<Switch {...defaultProps} />)
+        const sToggle = getByTestId('switch-toggle')
+
+        fireEvent.click(sToggle)
+
+        expect(defaultProps.setToggle).toHaveBeenCalled()
+        expect(defaultProps.setToggle).toHaveBeenCalledTimes(1)
+    })
+
+    describe('prop: toggle', () => {
+        it('is false, should render with gray color', () => {
+            const { getByTestId } = render(<Switch {...defaultProps} />)
+
+            const switchFather = getByTestId('switch-toggle')
+            const switchChildren = switchFather.firstChild as HTMLDivElement
+
+            expect(switchFather.className).toContain('bg-gray-300')
+            expect(switchChildren.className).toContain('bg-white')
+        })
+
+        it('is true, should render with blue color', () => {
+            const { getByTestId } = render(<Switch {...defaultProps} toggle={true} />)
+
+            const switchFather = getByTestId('switch-toggle')
+            const switchChildren = switchFather.firstChild as HTMLDivElement
+
+            expect(switchFather.className).toContain('bg-blue-100')
+            expect(switchChildren.className).toContain('bg-blue')
+        })
     })
 })
