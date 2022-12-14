3e7bfceff2d0087bcb1febe65caac8fbfd0f33fa
tbor00 <timoteo.borgobello@dd3tech.mx>
2022-12-14 18:29:06 -0300

    tests: add defaultProps on TextArea component

--- src/components/Form/TextArea.test.tsx
+++ src/components/Form/TextArea.test.tsx
@@ -1,44 +1,49 @@
-import { it, describe } from 'vitest'
+import { it, describe, vi } from 'vitest'
 import { fireEvent, render } from '@testing-library/react'
 
 import { TextArea } from './TextArea'
 
+const defProps = {
+    role: 'textarea',
+    onChange: vi.fn()
+}
+
 describe('<TextArea/>', () => {
     it('should be render', () => {
-        const { getByRole } = render(<TextArea role="textarea" />)
+        const { getByRole } = render(<TextArea {...defProps} />)
         expect(getByRole('textarea')).toBeDefined()
     })
 
     it('should render with label', () => {
         const lbl = 'Im a label'
-        const { getByText } = render(<TextArea role="textarea" label={lbl} />)
+        const { getByText } = render(<TextArea {...defProps} label={lbl} />)
         expect(getByText(lbl)).toBeDefined()
     })
 
     it('should render disabled', () => {
-        const { getByRole } = render(<TextArea role="textarea" disabled />)
+        const { getByRole } = render(<TextArea {...defProps} disabled />)
         expect(getByRole('textarea')).toBeDisabled()
     })
 
     it('should render without disabled', () => {
-        const { getByRole } = render(<TextArea role="textarea" />)
+        const { getByRole } = render(<TextArea {...defProps} />)
         expect(getByRole('textarea')).not.toBeDisabled()
     })
 
     it('should render with a custom maxLenght prop', async () => {
-        const { getByRole } = render(<TextArea role="textarea" maxlength={500} />)
+        const { getByRole } = render(<TextArea {...defProps} maxlength={500} />)
         const textArea = getByRole('textarea')
         expect(textArea.getAttribute('maxlength')).toBe('500')
     })
 
     it('should render with a custom value prop', async () => {
-        const { getByText } = render(<TextArea role="textarea" value={'Hello'} />)
+        const { getByText } = render(<TextArea {...defProps} value={'Hello'} />)
         const textArea = getByText('Hello')
         expect(textArea).toBeDefined()
     })
 
     it('should be able to be changed by event', () => {
-        const { getByRole } = render(<TextArea role="textarea" />)
+        const { getByRole } = render(<TextArea {...defProps} />)
         const textArea = getByRole('textarea') as HTMLInputElement
 
         fireEvent.change(textArea, { target: { value: 'Welcome' } })
