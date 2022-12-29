0ee8ec950f099f4bb4552b4501b60a43465f0142
JuanDD3 <juan.dominguez@dd3tech.mx>
2022-12-29 14:16:59 -0600

    fix(components): 🐛 Fixing mobile className in text component

--- src/components/Typography/Text.test.tsx
+++ src/components/Typography/Text.test.tsx
@@ -12,7 +12,7 @@ describe('<Text/>', () => {
     })
     it('should be responsive', () => {
         const { container } = render(<Text children="Responsive" size={{ sm: 'xs', md: '2xl', lg: '6xl', xl: '9xl', '2xl': 'sm' }} />)
-        expect(container.children[0]).toHaveClass('sm:text-xs md:text-2xl lg:text-6xl xl:text-9xl 2xl:text-sm')
+        expect(container.children[0]).toHaveClass('text-xs md:text-2xl lg:text-6xl xl:text-9xl 2xl:text-sm')
     })
     it('should have a number class', () => {
         const { container } = render(<Text children="Responsive" size={20} />)
