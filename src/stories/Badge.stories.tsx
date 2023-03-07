a961df7fccfadd03de878b5275ee45eb14f943a0
Lautaro Vilas <lautaro.vilas@dd3tech.mx>
2023-03-07 16:41:54 -0300

    feat: added RefreshIcon to Badge and create stories the Badge

---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import BadgeComponent from '../components/Badge/Badge'
+
+export default {
+    title: 'Components/Badge',
+    component: BadgeComponent
+} as ComponentMeta<typeof BadgeComponent>
+
+const Template: ComponentStory<typeof BadgeComponent> = (args) => <BadgeComponent {...args} />
+
+export const Badge = Template.bind({})
+Badge.args = {
+    variant: 'infoPrimary',
+    icon: 'HomeIcon',
+    text: 'Im a Badge',
+    classNameIcon: 'w-4 text-blue-600'
+}
