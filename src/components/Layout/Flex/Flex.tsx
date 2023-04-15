f9b41b1b4a4308b8c18e59fbcdb8e5d7b476ff27
tbor00 <tborgob@gmail.com>
2023-04-15 01:01:34 -0300

    feat: complement Flex component with more props

--- src/components/Layout/Flex/Flex.tsx
+++ src/components/Layout/Flex/Flex.tsx
@@ -1,17 +1,38 @@
 import React, { forwardRef } from 'react'
+import { composeClasses } from 'lib/classes'
+import { AlignItems, JustifyContent, Gap } from 'interfaces/types'
 
-interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
+  gap?: Gap
+  justifyContent?: JustifyContent
+  alignItems?: AlignItems
   className?: string
   children?: React.ReactNode
 }
 
 const Flex = forwardRef<HTMLDivElement, FlexProps>(
-  ({ className, children, ...props }: FlexProps, ref) => {
+  (
+    {
+      className,
+      alignItems = 'center',
+      justifyContent = 'between',
+      gap,
+      children,
+      ...props
+    }: FlexProps,
+    ref
+  ) => {
     return (
       <div
         ref={ref}
         role="flex"
-        className={`flex ${className ?? ''}`}
+        className={composeClasses(
+          'flex',
+          gap && `gap-${gap}`,
+          alignItems && `items-${alignItems}`,
+          justifyContent && `justify-${justifyContent}`,
+          className
+        )}
         {...props}
       >
         {children}
