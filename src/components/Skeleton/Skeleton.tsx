1700078d793608ca6c0e5102de8ca4a336ff1394
AntonioFeregrino <AntonioFeregrino@users.noreply.github.com>
2023-04-15 01:01:38 +0000

    ci: run linter

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -28,7 +28,10 @@ const Skeleton = ({
     <div
       data-testid="skeleton-content"
       className="animate-pulse"
-      style={{ width: inFlex ? '100%' : 'initial', height: inFlex ? '100%' : 'initial' }}
+      style={{
+        width: inFlex ? '100%' : 'initial',
+        height: inFlex ? '100%' : 'initial'
+      }}
     >
       <div
         data-testid="skeleton"
