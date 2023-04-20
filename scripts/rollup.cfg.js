188a4d7bde6d162c978d66cca3ffdec8c445981f
tbor00 <tborgob@gmail.com>
2023-04-19 21:09:16 -0300

    perf: add extra config to improve bundle size

--- scripts/rollup.cfg.js
+++ scripts/rollup.cfg.js
@@ -84,7 +84,15 @@ export function createModule({ plugins, format = 'esm', ...otherProps }) {
       sourcemap: true
     },
     plugins: plugins,
+    cache: true,
+    treeshake: {
+      moduleSideEffects: false
+    },
     external: ['react', 'react-dom'],
+    onwarn: (warning, warn) => {
+      // if (warning.code === 'CIRCULAR_DEPENDENCY') return
+      warn(warning)
+    },
     ...otherProps
   }
 
