05bfe6807513e3efd9f9e001547d1278487175a9
tbor00 <tborgob@gmail.com>
2023-05-17 18:05:49 -0300

    fix: change exports of the multi-ranger

--- src/components/RangeSlider/MultiRangeSlider.tsx
+++ src/components/RangeSlider/MultiRangeSlider.tsx
@@ -6,9 +6,9 @@ export type { RangeValues as IRangeSlider }
 /**
  * @deprecated Use `import { RangeSlider } from 'dd360-ds/RangeSlider'`
  */
-export default ((props: RangeSliderProps) => {
+export function MultiRangeSlider(props: RangeSliderProps) {
   console.warn(
     "[DEPRECATED] This component is deprecated. Instead use `import { RangeSlider } from 'dd360-ds/RangeSlider'` with a prop called multi."
   )
   return <RangeSlider {...props} multi />
-}) as typeof RangeSlider
+}
