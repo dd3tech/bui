dbfc5c305ab9c9e2d15ad732342313633a78c653
misael.arreola <misael.arreola@dd3tech.mx>
2023-05-29 22:41:02 -0600

    refactor: add import

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -12,3 +12,6 @@ export * from './Order'
 
 export { default as Overflow } from './Overflow'
 export * from './Overflow'
+
+export { default as Transition } from './Transition'
+export * from './Transition'
