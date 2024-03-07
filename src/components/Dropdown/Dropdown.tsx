10198bd93eb409da3483055737a772cd5229b504
carlos-yacante-dd3 <161601852+carlos-yacante-dd3@users.noreply.github.com>
2024-03-06 21:14:45 -0300

    fix: close dropdown when click outside (#190)
    fix: close dropdown when click outside

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -2,8 +2,16 @@
  * Copyright (c) DD360 and its affiliates.
  */
 
-import { PropsWithoutRef, ReactElement, RefAttributes, useState } from 'react'
+import {
+  PropsWithoutRef,
+  ReactElement,
+  RefAttributes,
+  useEffect,
+  useRef,
+  useState
+} from 'react'
 import { composeClasses } from 'lib/classes'
+
 import DropdownMenu from './DropdownMenu'
 import DropdownTrigger from './DropdownTrigger'
 import { DropdownProvider } from './DropdownContext'
@@ -17,12 +25,30 @@ interface DropdownProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Dropdown = ({ children, disableAnimation, ...props }: DropdownProps) => {
+  const ref = useRef<HTMLDivElement>(null)
   const [toggle, setToggle] = useState(false)
   const [trigger, menu] = children
 
+  useEffect(() => {
+    const handleClickOutside = (ev: any) => {
+      if (ref.current && !ref.current.contains(ev.target)) {
+        setToggle(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
     <DropdownProvider value={{ disableAnimation, toggle, setToggle }}>
-      <div {...props} className={composeClasses('relative', props.className)}>
+      <div
+        ref={ref}
+        {...props}
+        className={composeClasses('relative', props.className)}
+      >
         {trigger}
         {menu}
       </div>
