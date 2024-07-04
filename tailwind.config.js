71cd6be03e08e9136713089d2b39bb54cdc1afb3
ariel_rodriguez <ariel.rodriguez@dd3tech.mx>
2024-07-04 19:49:45 -0300

    fix: test tailwind css
    test tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: {
-     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], 
-  },
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], 
+  content: ['./src/**/*.{js,jsx,ts,tsx,css}', './public/index.html'],
   darkMode: 'class',
   theme: {
     fontFamily: {
