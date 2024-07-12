d1527b3bbea3cc00f2d414b35712a35be633e0e4
‘ariel_rodriguez” <ariel.rodriguez@dd3tech.mx>
2024-07-12 18:11:42 -0300

    fix: remove index to key
    remove index to key

--- src/components/Kbd/Kbd.tsx
+++ src/components/Kbd/Kbd.tsx
@@ -1,8 +1,7 @@
 /*
  * Copyright (c) DD360 and its affiliates.
  */
-
-import React, { forwardRef, LegacyRef } from 'react'
+import React, { forwardRef, LegacyRef, useRef, useCallback } from 'react'
 
 export interface KBDProps {
   kbds: Array<string>
@@ -11,26 +10,36 @@ export interface KBDProps {
 
 const Kbd = forwardRef<HTMLElement, KBDProps>(
   ({ kbds = ['Ctrl', 'Shift', 'R'], separator = '+' }: KBDProps, ref) => {
-    const isLastKbd = React.useCallback(
+    const keyCounter = useRef(0)
+
+    const isLastKbd = useCallback(
       (indexOfKey: number) => {
         return kbds.length - 1 === indexOfKey
       },
       [kbds]
     )
 
+    const createKey = () => {
+      keyCounter.current += 1
+      return keyCounter.current
+    }
+
     return (
       <p
         className="text-info dark:text-gray-400"
         ref={ref as LegacyRef<HTMLParagraphElement>}
       >
-        {kbds.map((text, index) => (
-          <React.Fragment key={`${index}-text`}>
-            <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-info">
-              {text}
-            </kbd>{' '}
-            <span>{!isLastKbd(index) && separator} </span>
-          </React.Fragment>
-        ))}
+        {kbds.map((text) => {
+          const mockedId = createKey()
+          return (
+            <React.Fragment key={mockedId}>
+              <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-info">
+                {text}
+              </kbd>{' '}
+              <span>{!isLastKbd(kbds.indexOf(text)) && separator} </span>
+            </React.Fragment>
+          )
+        })}
       </p>
     )
   }
