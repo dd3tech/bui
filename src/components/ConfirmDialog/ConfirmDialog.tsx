c9ee6a43807da94ba23d055ceed52f3cbaefeccd
ariel_rodriguez <ariel.rodriguez@dd3tech.mx>
2024-08-06 13:44:11 -0300

    fix: remove position confirm dialog
    remove position confirm dialog

--- src/components/ConfirmDialog/ConfirmDialog.tsx
+++ src/components/ConfirmDialog/ConfirmDialog.tsx
@@ -16,7 +16,6 @@ export interface IConfirmDialog {
   textCancelBtn?: string
   onConfirm: () => void
   onCancel?: () => void
-  position?: { show: boolean; left: number; top: number }
   className?: string
   width?: number | string
   idRoot?: string
@@ -27,7 +26,6 @@ const ConfirmDialog = ({
   children,
   textConfirmBtn = 'Apply',
   textCancelBtn = 'Reset',
-  position,
   className,
   width,
   onConfirm,
@@ -36,45 +34,42 @@ const ConfirmDialog = ({
 }: IConfirmDialog) => {
   return (
     <Portal idRoot={idRoot}>
-      {position?.show && (
-        <Card
-          rounded="lg"
-          className={composeClasses('w-max absolute bg-white', className)}
-          width={width}
-          style={{ left: position?.left, top: position?.top }}
-        >
-          {title && (
-            <Text variant="p" className="text-info mb-4 text-xxs font-semibold">
-              {title}
-            </Text>
-          )}
+      <Card
+        rounded="lg"
+        className={composeClasses('w-max absolute bg-white', className)}
+        width={width}
+      >
+        {title && (
+          <Text variant="p" className="text-info mb-4 text-xxs font-semibold">
+            {title}
+          </Text>
+        )}
 
-          {children}
+        {children}
 
-          <div className="flex gap-4 justify-end mt-1.5">
-            {onCancel && (
-              <Button
-                role="cancel-btn"
-                variant="link"
-                className="underline text-xs disabled:opacity-75 text-black"
-                style={{ padding: 0, fontWeight: 600 }}
-                onClick={onCancel}
-              >
-                {textCancelBtn}
-              </Button>
-            )}
+        <div className="flex gap-4 justify-end mt-1.5">
+          {onCancel && (
             <Button
-              role="confirm-btn"
+              role="cancel-btn"
               variant="link"
-              className="underline text-xs disabled:opacity-75 text-blue-500"
+              className="underline text-xs disabled:opacity-75 text-black"
               style={{ padding: 0, fontWeight: 600 }}
-              onClick={onConfirm}
+              onClick={onCancel}
             >
-              {textConfirmBtn}
+              {textCancelBtn}
             </Button>
-          </div>
-        </Card>
-      )}
+          )}
+          <Button
+            role="confirm-btn"
+            variant="link"
+            className="underline text-xs disabled:opacity-75 text-blue-500"
+            style={{ padding: 0, fontWeight: 600 }}
+            onClick={onConfirm}
+          >
+            {textConfirmBtn}
+          </Button>
+        </div>
+      </Card>
     </Portal>
   )
 }
