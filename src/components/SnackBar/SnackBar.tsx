6e443861a215f7e7901d94161b1bd141d22b010c
ariel_rodriguez <ariel.rodriguez@dd3tech.mx>
2024-12-09 12:19:06 -0300

    fix: a portal was added
    a portal was added

--- src/components/SnackBar/SnackBar.tsx
+++ src/components/SnackBar/SnackBar.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import DynamicHeroIcon from 'common/DynamicHeroIcon'
+import { InformationCircleIcon } from '@heroicons/react/solid'
 import Button from 'components/Buttons/Button'
 import Card from 'components/Card/Card'
 import Flex from 'components/Layout/Flex/Flex'
 import Text from 'components/Typography'
+import Portal from 'common/Portal/Portal'
 import { composeClasses } from 'lib/classes'
 import './snackBar.css'
 
@@ -14,7 +16,7 @@ interface SnackBarProps {
     icon?: keyof typeof DynamicHeroIcon
   }
   description: string
-  buttonPrimary: {
+  buttonPrimary?: {
     label: string
     icon?: keyof typeof DynamicHeroIcon
     position: 'right' | 'left'
@@ -26,6 +28,7 @@ interface SnackBarProps {
     position: 'right' | 'left'
     onClick: () => void
   }
+  className?: string
 }
 
 const SnackBar = ({
@@ -33,7 +36,8 @@ const SnackBar = ({
   description,
   buttonPrimary,
   buttonGhost,
-  show
+  show,
+  className
 }: SnackBarProps) => {
   const [isVisible, setIsVisible] = useState(show)
 
@@ -53,53 +57,62 @@ const SnackBar = ({
   if (!isVisible) return null
 
   return (
-    <div
-      className={composeClasses(
-        'fixed bottom-14 z-[9999] w-full max-w-6xl px-4',
-        show ? 'animate-fadeInUp' : 'animate-fadeOutUp'
-      )}
-    >
-      <Card rounded="2xl" className="shadow-xl bg-white">
-        <Flex alignItems="center" gap="2">
-          <DynamicHeroIcon
-            icon={title.icon || 'InformationCircleIcon'}
-            className="w-4 h-4 text-gray-700"
-          />
-          <Text size="base">{title.label}</Text>
-        </Flex>
-        <Flex className="mt-2">
-          <Text size="sm">{description}</Text>
-        </Flex>
-        <Flex justifyContent="end" gap="2" className="mt-2">
-          {buttonGhost && (
-            <Button
-              onClick={buttonGhost.onClick}
-              variant="ghost"
-              className="flex gap-2 items-center px-6"
-            >
-              {buttonGhost.position === 'left' &&
-                SnackBarButton(buttonGhost.icon)}
-              <Text size="xs">{buttonGhost.label}</Text>
-              {buttonGhost.position === 'right' &&
-                SnackBarButton(buttonGhost.icon)}
-            </Button>
-          )}
-          {buttonPrimary && (
-            <Button
-              onClick={buttonPrimary.onClick}
-              variant="primary"
-              className="flex gap-2 items-center px-6"
-            >
-              {buttonPrimary.position === 'left' &&
-                SnackBarButton(buttonPrimary.icon)}
-              <Text size="xs">{buttonPrimary.label}</Text>
-              {buttonPrimary.position === 'right' &&
-                SnackBarButton(buttonPrimary.icon)}
-            </Button>
-          )}
-        </Flex>
-      </Card>
-    </div>
+    <Portal>
+      <div
+        className={composeClasses(
+          className ??
+            'fixed bottom-8 left-60 w-full max-w-4xl 2xl:max-w-6xl px-4',
+          show ? 'animate-fadeInUp' : 'animate-fadeOutUp'
+        )}
+        style={{ zIndex: 10000 }}
+      >
+        <Card rounded="2xl" className="shadow-xl bg-white">
+          <Flex alignItems="center" gap="2">
+            {title.icon ? (
+              <DynamicHeroIcon
+                icon={title.icon || 'InformationCircleIcon'}
+                className="w-4 h-4 text-gray-700"
+              />
+            ) : (
+              <InformationCircleIcon className="w-4 h-4" />
+            )}
+
+            <Text size="base">{title.label}</Text>
+          </Flex>
+          <Flex className="mt-2">
+            <Text size="sm">{description}</Text>
+          </Flex>
+          <Flex justifyContent="end" gap="2" className="mt-2">
+            {buttonGhost && (
+              <Button
+                onClick={buttonGhost.onClick}
+                variant="ghost"
+                className="flex gap-2 items-center px-6"
+              >
+                {buttonGhost.position === 'left' &&
+                  SnackBarButton(buttonGhost.icon)}
+                <Text size="xs">{buttonGhost.label}</Text>
+                {buttonGhost.position === 'right' &&
+                  SnackBarButton(buttonGhost.icon)}
+              </Button>
+            )}
+            {buttonPrimary && (
+              <Button
+                onClick={buttonPrimary.onClick}
+                variant="primary"
+                className="flex gap-2 items-center px-6"
+              >
+                {buttonPrimary.position === 'left' &&
+                  SnackBarButton(buttonPrimary.icon)}
+                <Text size="xs">{buttonPrimary.label}</Text>
+                {buttonPrimary.position === 'right' &&
+                  SnackBarButton(buttonPrimary.icon)}
+              </Button>
+            )}
+          </Flex>
+        </Card>
+      </div>
+    </Portal>
   )
 }
 
